ce7d55442ed381019156a6177050e17c2becdd55
Anas faisi <anasfaisi008@gmail.com>
2025-03-24 11:43:37 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/ReactTodo/', // Replace with your actual GitHub repository name
   plugins: [react()],
 })
